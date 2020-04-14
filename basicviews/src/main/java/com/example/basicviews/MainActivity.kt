d51ce3bfd340058c9585c9964520c85d2909c0ba
akurkov <akurkov@mail.ru>
2020-04-14 16:40:26 +0300

    product

--- basicviews/src/main/java/com/example/basicviews/MainActivity.kt
+++ basicviews/src/main/java/com/example/basicviews/MainActivity.kt
@@ -13,29 +13,40 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Получение настроек из файла настроек
         val spref = getPreferences(Context.MODE_PRIVATE)
         rbJSON.isChecked = spref.getBoolean("rbJSON",false)
         rbHTML.isChecked = spref.getBoolean("rbHTML",true)
+        tFind.setText(spref.getString("Find",""))
     }
 
     override fun onDestroy() {
         super.onDestroy()
+
+        // Сохранение настроек в файл настроек
         val spref = getPreferences(Context.MODE_PRIVATE)
         val ed = spref.edit()
         ed.putBoolean("rbJSON",rbJSON.isChecked)
         ed.putBoolean("rbHTML",rbHTML.isChecked)
+        ed.putString("Find",tFind.text.toString())
         ed.commit()
     }
 
+    // Обработка нажатия кнопки поиска
     fun onbFindClick(view: View){
         if (rbHTML.isChecked) {
+            // Для формата HTML отображается страница sql.php
             val intent = Intent(this@MainActivity, browser::class.java)
             intent.putExtra("uri","http://l90268zm.beget.tech/sql.php?query="+tFind.text.toString())
+            intent.putExtra("texttype","HTML")
             startActivity(intent)
         }
         else{
-            val intent = Intent(this@MainActivity, JSON::class.java)
+            // Для формата JSON идет обращение к странице json.php
+            val intent = Intent(this@MainActivity, browser::class.java)
             intent.putExtra("uri","http://l90268zm.beget.tech/json.php?query="+tFind.text.toString())
+            intent.putExtra("texttype","JSON")
             startActivity(intent)
         }
     }
--- basicviews/src/main/java/com/example/basicviews/browser.kt
+++ basicviews/src/main/java/com/example/basicviews/browser.kt
@@ -1,18 +1,114 @@
 package com.example.basicviews
 
 import android.os.Bundle
+import android.view.View
 import android.webkit.WebViewClient
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import io.ktor.client.HttpClient
+import io.ktor.client.features.defaultRequest
+import io.ktor.client.request.get
+import io.ktor.client.request.header
 import kotlinx.android.synthetic.main.activity_browser.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import org.json.JSONArray
 
 class browser : AppCompatActivity() {
+
+    var JSONArray = JSONArray() // Глобальный массив с данными, куда закачиваются данные при первом поиске. Нужно для перемещения между записями JSON-массива
+    var JSONArrayIndex = 0 // Номер текущей записи массива
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_browser)
+
+        // Из интента получаем URL и тип вывода текста
         val uri = intent.getStringExtra("uri")
-        wvText.settings.javaScriptEnabled = true
-        wvText.loadUrl(uri)
-        wvText.webViewClient = WebViewClient()
+        val texttype = intent.getStringExtra("texttype")
+        when(texttype){
+            "HTML" -> {
+                // страница HTML просто подключается к WebView
+                wvText.tag = "HTTP"
+                wvText.settings.javaScriptEnabled = true
+                wvText.loadUrl(uri)
+                wvText.webViewClient = WebViewClient()
+                btBack.visibility = ImageButton.GONE
+                btForward.visibility = ImageButton.GONE
+            }
+            "JSON" ->{
+                btBack.visibility = ImageButton.VISIBLE
+                btForward.visibility = ImageButton.VISIBLE
+                // Асинхронный вызов получения JSON-данных и формаирование JSON-массива
+                val job = GlobalScope.async(Dispatchers.Main) {
+                    val client = HttpClient() {
+                        defaultRequest {
+                            header(
+                                "User-Agent",
+                                "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:55.0) Gecko/20100101 Firefox/55.0"
+                            )
+                        }
+                    }
+                    val stRequest = "[" + client.get<String>(uri).substringAfter("[", "[]")
+                    JSONArray = JSONArray(stRequest)
+                    wvText.tag = "JSON"
+                    fSetWebView()
+                }
+            }
+        }
+
+    }
+
+    fun onbtForwardClick(view: View){
+        // При нажатии на кнопку Вперед увеличивается индекс записи JSON-массива и обновляется WebView
+        if (wvText.tag == "JSON"){
+            if (JSONArrayIndex < (JSONArray.length()-1)){
+                JSONArrayIndex = JSONArrayIndex + 1
+                fSetWebView()
+            }
+        }
+    }
+
+    fun onbtBackClick(view: View){
+        // При нажатии на кнопку Назад уменьшается индекс записи JSON-массива и обновляется WebView
+        if (wvText.tag == "JSON"){
+            if (JSONArrayIndex > 0){
+                JSONArrayIndex = JSONArrayIndex - 1
+                fSetWebView()
+            }
+        }
+    }
+
+    // Перегрузка обработчика аппаратной кнопки Назад для хождения внутри WebView
+    override fun onBackPressed() {
+        if (wvText.canGoBack()){
+            wvText.goBack()
+        }
+        else {
+            super.onBackPressed()
+        }
+    }
+
+    // Вывод текущей записи JSON-массива в виде таблицы HTML
+    fun fSetWebView(){
+        if (JSONArray.length()>0){
+            var stHTML = "<head></head><body><table border=\"1\">"
+            for (j in 0..(JSONArray.getJSONObject(JSONArrayIndex).names().length() - 1)) {
+                stHTML = stHTML + "<tr>"
+                stHTML = stHTML + "<td>" + JSONArray.getJSONObject(JSONArrayIndex).names().getString(j) + "</td>"
+                if (JSONArray.getJSONObject(JSONArrayIndex).names().getString(j) == "Изображения") {
+                    stHTML = stHTML + "<td><a href=\"" + JSONArray.getJSONObject(JSONArrayIndex).getString(JSONArray.getJSONObject(JSONArrayIndex).names().getString(j)
+                        ) + "\">Изображение</a></td>"
+                } else {
+                    stHTML = stHTML + "<td>" + JSONArray.getJSONObject(JSONArrayIndex).getString(JSONArray.getJSONObject(JSONArrayIndex).names().getString(j)
+                        ) + "</td>"
+                }
+                stHTML = stHTML + "</tr>"
+            }
+            stHTML = stHTML + "</table></body>"
+            wvText.loadData(stHTML, "text/html", "UTF-8")
+            wvText.webViewClient = WebViewClient()
+        }
     }
 }
